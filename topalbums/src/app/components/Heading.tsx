2196b5ab2021437b5ee179abe94ed74c53625c9d
Steven Hanson <stevenahanson@live.com>
2023-10-20 15:20:16 +0100

    Add heading

---
+++ topalbums/src/app/components/Heading.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Heading() {
+  return (
+    <div className="flex items-center justify-center w-full h-full">
+      <p className="text-slate-700 font-sans text-4xl p-16">My top album!</p>
+    </div>
+  );
+}
--- topalbums/src/app/page.tsx
+++ topalbums/src/app/page.tsx
@@ -1,3 +1,5 @@
+import Heading from "./components/Heading";
+
 export default function Home() {
-  return <p>Hello World</p>;
+  return <Heading />;
 }
